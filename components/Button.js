52b5e39b1a9396b47ac4fab5154965751d5b6eae
uelionobre <uelionobre@hotmail.com>
2024-06-16 07:37:01 -0300

    Adiciona um botão primário

--- components/Button.js
+++ components/Button.js
@@ -1,18 +1,36 @@
-import { StyleSheet, View, Pressable, Text } from 'react-native'
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
-export default function Button({ label }) {
-  return (
-    <View
-      style={styles.buttonContainer}
-    >
-      <Pressable
-        style={styles.button}
-        onPress={
-          () => alert('Voce pressionou um botão')
-        }
+export default function Button({ label, theme }) {
+  if (theme == "primary") {
+    return (
+      <View
+        style={[styles.buttonContainer, { borderWidth: 4, borderColor: "#ffd33d", borderRadius: 18 }]}
       >
+        <Pressable
+          style={[styles.button, { backgroundColor: "#fff" }]}
+          onPress={
+            () => alert('Voce pressionou um botão')
+          }
+        >
+          <FontAwesome
+            name="picture-o"
+            size={18}
+            color="#25292e"
+            style={styles.buttonIcon}
+          />
+          <Text style={[styles.buttonLabel, { color: "#25292e" }]}>{label}</Text>
+        </Pressable>
+      </View>
+    )
+  }
+
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={() => alert("Voce pressionou um botão")}>
         <Text style={styles.buttonLabel}>{label}</Text>
       </Pressable>
+
     </View>
   )
 }
@@ -27,10 +45,9 @@ const styles = StyleSheet.create({
     padding: 3,
   },
   button: {
+    borderRadius: 10,
     width: '100%',
     height: '100%',
-    borderRadius: 10,
-    borderWidth: 3,
     alignItems: 'center',
     justifyContent: 'center',
     flexDirection: 'row'
@@ -39,7 +56,7 @@ const styles = StyleSheet.create({
     paddingRight: 8
   },
   buttonLabel: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 16
   }
 });
