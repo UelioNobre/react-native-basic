eb95ae613d939ac5ee17b88011f563dc811a514e
uelionobre <uelionobre@hotmail.com>
2024-06-16 07:53:50 -0300

    Adiciona abertura de imagem usando a biblioteca expo-image-picker

--- App.js
+++ App.js
@@ -2,17 +2,31 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View, Image } from 'react-native';
 import ImageViewer from './components/ImageViewer';
 import Button from './components/Button';
+import * as ImagePicker from 'expo-image-picker';
 
 const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
+  const pickImageAsync = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1
+    });
+
+    if (!result.canceled) {
+      console.log(result);
+    } else {
+      alert('Você não selecionou nenhum imagem.');
+    }
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.imageContainer}>
         <ImageViewer placeholderImageSource={PlaceholderImage} />
       </View>
       <View style={styles.footerContainer}>
-        <Button theme="primary" label="Escolha uma foto" />
+        <Button theme="primary" onPress={pickImageAsync} label="Escolha uma foto" />
         <Button label="Usar esta foto" />
       </View>
       <StatusBar style="auto" />
